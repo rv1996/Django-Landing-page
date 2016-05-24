05258bd78e393d96a6446f76547a4a30c20e02eb
Rupanshu <vermarupanshu733@gmail.com>
2016-05-24 17:29:19 +0530

    Functionality of collection poeple names and there email is completed
    	*validation in forms is done
    	*data base is updating
    	*for now , same page after submiting with updated context variable

--- mvp/views.py
+++ mvp/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from .forms import SignUpForm
 def home(request):
-    title = "Home Page"
 
+    if request.user.is_authenticated():
+        title = "Home|{}".format(request.user)
+    else :
+        title = "Home"
+    form = SignUpForm()
     context_variable = {
-        "template_title":title
+        "template_title": title,
+        "form": form,
     }
+
+    #add a form
+
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST or None)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            context_variable = {
+                "title": title,
+                "comment": "Thank you submitting",
+            }
+            print (instance.email)
+            print (instance.timestamp)
+            print (instance.full_name)
+            print ("form is valid")
+
+        print("POST call ")
+
+
     return render(request,'mvp/home.html',context_variable)
